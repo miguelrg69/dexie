59bbca978aa31e0527d7fa2bfa2e5c21bcae55bf
miguelrg69 <67283102+miguelrg69@users.noreply.github.com>
2022-06-16 17:45:47 -0500

    cambios

--- src/app/database.ts
+++ src/app/database.ts
@@ -3,17 +3,93 @@ import Dexie from "dexie";
 
 export class obrasDB extends Dexie{
     obras: Dexie.Table<IObra,number>
+    registros: Dexie.Table<Iregistro,number>
     constructor(){
         super("obrasDB");
 
         this.version(1).stores({
-            obras: '++id, titulo, imageURL'
+            obras: '++id, titulo, imageURL',
+            registros: '++id, placasT, placasURL, fechaHora, cantidad, material, camionLlegada, camionCargado, camionDescargando, camionDescargado, hora, firma, unidadMed, obraId -> obras.id'
         })
 
 
         this.obras.mapToClass(Obras)
     }
 
+}
+
+export interface Iregistro{
+    //llegada del camion
+    id?: number,
+    placas: string,
+    placasURL:string,
+    fechaHora: string,
+    material: string,
+    cantidad: number,
+    unidadMed: string
+    //registro del suministro
+    camionLlegada: string,
+    camionCargado: string,
+    camionDescargando: string,
+    camionDescargado: string,
+    hora: string,
+    //confirmacion del registro
+    firma: string
+    obraId: number
+}
+
+export class registros implements Iregistro{
+        //llegada del camion
+        id?: number;
+        placas: string;
+        placasURL:string;
+        fechaHora: string;
+        material: string;
+        cantidad: number;
+        unidadMed: string;
+        //registro del suministro
+        camionLlegada: string;
+        camionCargado: string;
+        camionDescargando: string;
+        camionDescargado: string;
+        hora: string;
+        //confirmacion del registro
+        firma: string;
+        obraId: number;
+        constructor(placas: string,
+            placasURL:string,
+            fechaHora: string,
+            material: string,
+            cantidad: number,
+            unidadMed: string,
+            //registro del suministro
+            camionLlegada: string,
+            camionCargado: string,
+            camionDescargando: string,
+            camionDescargado: string,
+            hora: string,
+            //confirmacion del registro
+            firma: string,
+            obraId: number, id?: number){
+
+                this.placas = placas
+                this.placasURL = placasURL
+                this.fechaHora = fechaHora
+                this.material = material
+                this.cantidad = cantidad
+                this.unidadMed = unidadMed
+                this.camionLlegada = camionLlegada
+                this.camionDescargando = camionDescargando
+                this.camionCargado = camionCargado
+                this.camionDescargado = camionDescargado
+                this.hora = hora
+                this.firma = firma
+                this.obraId = obraId
+            }
+
+            saveR(){
+                return db.registros.add(this)
+            }
 }
 
 export interface IObra{
